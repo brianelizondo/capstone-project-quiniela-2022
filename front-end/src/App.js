d3bfdf3b0905f4f4d3989b1507703fb7264068c6
Brian Elizondo <brian_elizondo@msn.com>
2022-10-26 19:46:08 -0400

    Capstone Project => Step Four - Coding User Flows: Front-End -> config initial files, import initial libraries and initial navbar with possible components needed by routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,62 @@
 import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+
 import './App.css';
+
+// Import files to routes
+
 
 function App() {
     return (
-        <div className="App">
-        </div>
+        <BrowserRouter>
+            <NavBar />
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                
+                {/* ROUTES TO PROTECT LATER */}
+                <Route exact path="/quinielas/:username/add">
+                    <QuinielaAddForm />
+                </Route>
+                <Route exact path="/quinielas/:username/update/:id">
+                    <QuinielaUpdateForm />
+                </Route>
+                <Route exact path="/quinielas/:username/:id">
+                    <QuinielaDetails />
+                </Route>
+                
+                <Route exact path="/groups">
+                    <MatchesGroups />
+                </Route>
+                <Route exact path="/matches">
+                    <Matches />
+                </Route>
+                <Route exact path="/matches/:id">
+                    <Matches />
+                </Route>
+                <Route exact path="/teams">
+                    <MatchesTeams />
+                </Route>
+                <Route exact path="/teams/:shortname">
+                    <MatchesTeams />
+                </Route>
+                <Route exact path="/rules">
+                    <Rules />
+                </Route>
+
+                <Route exact path="/register">
+                    <RegisterForm />
+                </Route>
+                <Route exact path="/login">
+                    <LoginForm />
+                </Route>
+
+                <Route>
+                    <PageNotFound />
+                </Route>
+            </Switch>
+        </BrowserRouter>
     );
 }
 
